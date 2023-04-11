56f061c7cd90ba21b6e14f42335855e973e9602c
orishaba-mercy <orishabamercybanga@gmail.com>
2023-04-11 09:01:50 +0300

    first commit

---
+++ assign.js
@@ -0,0 +1,61 @@
+//  Write a function that takes in a string and returns it when reversed
+function name(food){
+let food1 = 'eating';
+}
+console.log(food1.reverse());
+
+
+
+// // Write a function that takes in the following array and consoles the target if it is found else
+// // null
+//  let num = [2,8,0,23,5,45,76]
+ Target = 23
+ function binary(num){
+  let left =0;
+    let right=num.length-1
+    if (left[0]<=right[0]){
+     let middle= Math.floor(left+right/2)
+      let left =(num.slice(0,middle))
+      let right =(num.slice(middle))
+    }
+     
+    while(left.length&&right.length){
+     if(num[middle]===target);
+     return middle
+
+    }
+    if (num[middle]<=target){
+     left =middle+1
+   }
+    else{
+     right =middle-1
+    }
+    return null
+
+  }
+  let num = [2,8,0,23,5,45,76]
+  Target = 23
+  console.log(num(Target))
+
+// //  Given years between 2000 and 2023, console all the leap years in the following
+// // sentence, i.e “2020 is a leap year” if not console log i.e “2001 is not a leap year”
+
+// // Given a range of numbers from 0 to 100, console”Fizz” if the numbers are divisible by 3,
+// // “Buzz” if the numbers are divisible by 5, and “FizzBuzz” if divisible by both 3 and 5.
+
+
+
+
+// // 5. Write a function that takes in an array of numbers and returns an array that has all
+// // elements multiplied by 4.
+  let numArray = [12,87,45,75,23,64,73]
+  let array=[]
+ array.map(function(item){
+     return numArray*4
+  })
+  console.log(array)
+
+// // 6. Write a function that takes in an array of strings and returns an array with every element
+//  turned into a number
+  let nums = ['10','24','45','56','67']
+
